3c27f5ff82400316139b6ffbcf431072b26ef7a4
unknown <xuanphuong7797@gmail.com>
2018-08-16 13:41:58 +0700

    Add Migration table

---
+++ database/migrations/2018_08_16_043343_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 50)->unique();
+            $table->string('avatar');
+            $table->string('email', 100)->unique();
+            $table->string('gender');
+            $table->string('password');      
+            $table->tinyInteger('status')->comment('1 block, 0 normal');
+            $table->integer('role');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
