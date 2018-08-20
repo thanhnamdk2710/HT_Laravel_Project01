e7203eed2b930d713f7524772dbc90e91eca9557
unknown <xuanphuong7797@gmail.com>
2018-08-20 11:01:36 +0700

    Fix migrate v2

--- database/migrations/2018_08_16_083346_add_alias_to_books_table.php
+++ database/migrations/2018_08_16_083346_add_alias_to_books_table.php
@@ -26,7 +26,7 @@ class AddAliasToBooksTable extends Migration
     public function down()
     {
         Schema::table('books', function (Blueprint $table) {
-            //
+             $table->dropColumn('alias');
         });
     }
 }
