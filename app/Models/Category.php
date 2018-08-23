0b4e00ee0a429ef9ef0daa4156de05cbc0861840
thuytientranthi <tt.thuytien15@gmail.com>
2018-08-23 10:33:43 +0700

    Insert Category

---
+++ app/Models/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $filltable = ['name'];
+
+    public function books()
+    {
+    	$this->hasMany('App\Models\Book','category_id','id');
+    }
+}
---
+++ resources/views/backend/categories/insert.blade.php
@@ -0,0 +1,24 @@
+@extends('backend.layouts.master')
+
+@section('title', 'List Categories')
+
+@section('content')
+<section class="content-header">
+	<h1>Category table</h1>
+	<ol class="breadcrumb">
+		<li>
+			<a href="#">
+				<i class="fas fa-tachometer-alt"></i> Home
+			</a>
+		</li>
+		<li class="active">Categories</li>
+	</ol>
+</section>
+<!-- Main content -->
+<section class="content">
+	<!-- Small boxes (Stat box) -->
+	<div class="row">
+	</div>
+</section>
+<!-- /.content -->
+@endsection
