1f346ef6d26e47d698a43e8489c44f6c53761184
unknown <xuanphuong7797@gmail.com>
2018-09-05 14:28:35 +0700

    Fix seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        $this->call(CategoryTableSeeder::class);
+       $this->call(UsersTableSeeder::class);
+       $this->call(BooksTableSeeder::class);
+       $this->call(AuthorsTableSeeder::class);
+       $this->call(Authors_BooksTableSeeder::class);
+       $this->call(RatingsTableSeeder::class);
+       $this->call(TagsTableSeeder::class);
+       $this->call(Tags_BooksTableSeeder::class);
     }
 }
