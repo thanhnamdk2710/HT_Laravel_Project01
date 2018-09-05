aadcb1601e0d7ead662ea4679a9cbdd3a1d51580
unknown <xuanphuong7797@gmail.com>
2018-09-05 23:22:43 +0700

    Fix DB migration

--- database/migrations/2018_09_05_231715_add_author_to_books_table.php
+++ database/migrations/2018_09_05_231715_add_author_to_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAuthorsTable extends Migration
+class AddAuthorToBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateAuthorsTable extends Migration
      */
     public function up()
     {
-        Schema::create('authors', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
+        Schema::table('books', function (Blueprint $table) {
+            $table->string('author')->after('image');
         });
     }
 
@@ -27,6 +25,8 @@ class CreateAuthorsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('authors');
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('author');
+        });
     }
 }
