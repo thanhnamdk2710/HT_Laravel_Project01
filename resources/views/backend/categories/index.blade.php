8e3869e7d6755b23bac1b5223aedabe419052893
thuytientranthi <tt.thuytien15@gmail.com>
2018-09-10 14:35:58 +0700

    fix crud category

--- resources/views/backend/categories/index.blade.php
+++ resources/views/backend/categories/index.blade.php
@@ -39,6 +39,17 @@
 				</div>
 			</div>
 			@endif
+			@if (Session::has('error'))
+			<div class="box-header with-border">
+				<div class="col-md-6">
+					<div class="alert alert-danger alert-dismissible">
+						<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+						<h4><i class="icon fa fa-check"></i> Error!</h4>
+						<p>* {{ Session::get('error') }}</p>
+					</div>
+				</div>
+			</div>
+			@endif
 			<!-- /.box-header -->
 			<div class="box-body">
 				@include('backend.layouts.partials.modal')
